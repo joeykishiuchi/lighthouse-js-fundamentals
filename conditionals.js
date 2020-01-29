bae3fc295f57d218c07ffba86b16f344accf0bcd
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-01-29 05:36:55 +0000

    Use a for loop to repeat a chorus line

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,6 @@
-const temperature = 50;
+/*const temperature = 50;
 
-if (temperature < 0) {
+if (temperature < 0 ) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
@@ -9,3 +9,20 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+} */
+const whichSchool  = function (age) {
+    if(age < 13){
+      return 'Elementary School';
+    } else if(age >= 13 || age <= 18){
+      return 'Secondary School';
+    } else {
+      return 'Lighthouse Labs';
+    }
+}
+console.log(whichSchool(19));
